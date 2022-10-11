df1fda2af40b6865a25699eec49121d51c2a8568
SMAN 4 Muara Teweh <94159177+smafourmute@users.noreply.github.com>
2022-10-12 05:02:21 +0700

    Add files via upload

---
+++ template/sidebar.php
@@ -0,0 +1,5 @@
+<div class="py-3">
+	<h2>SMAN 4 Muara Teweh QR Generator</h2>
+	<p>Proyek gabut lainnya yang ada disini , ini terbuka untuk umum ,silahkan dipergunakan,pengembang sudah sebisa mungkin untuk membuat se user friendly mungkin.</p>
+	<a href="https://www.denso-wave.com/"><p>QR kode ini dibawah naungan hak cipta oleh DENSO WAVE INCORPORATED.</p></a>
+</div>
